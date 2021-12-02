534da231559924ff719ecabbad231407d8ef0b51
Egor <hor.2021.reyn@gmail.com>
2021-12-02 19:27:32 +0200

    first commit

---
+++ 1-fixed.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+  int first_num = 2;
+  int second_num = 3;
+  int sum = first_num + second_num;
+  printf("%d\n", sum);
+}
---
+++ 3-something.py
@@ -0,0 +1,12 @@
+def product(x, y):
+    return x * y
+
+def add(x, y):
+    return x + y
+
+first_num = 2
+second_num = 3
+prod = product(first_num, second_num)
+summ = add(first_num, second_num)
+print(summ)
+print(prod)
